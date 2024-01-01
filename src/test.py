712d5c619b4a73c0a9a40c33bc25ed14809e657d
ZorbMax <orban.maxime@gmail.com>
2024-01-01 18:59:14 +0100

    Stationary distribution first try

--- src/test.py
+++ src/test.py
@@ -1,23 +1,28 @@
 from math import exp
 
 import numpy as np
+import sys
+
+from matplotlib import pyplot as plt
 
 import pgg_game
 
+np.set_printoptions(threshold=sys.maxsize)
+
 # i = [[115, 35], [35, 15]]
-Z = 70
-Zr = 50
-Zp = 20
+Z = 100
+Zr = 20
+Zp = 80
 Z_tot = [Zr, Zp]
 N = 6
-mu = 1
-h = 0.5
+mu = 1/Z
+h = 1
 beta = 0.1
 
 b_r = b_p = 0.1
 c_r = c_p = 0.1
 Mcb_threshold = 6
-r = 3
+r = 0.2
 
 T_coop_rich = 1
 T_defect_rich = 1
@@ -71,7 +76,6 @@ def compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h):
 
 M = np.zeros([(Zp + 1) * (Zr + 1), (Zp + 1) * (Zr + 1)])
 for rich_coop in range(Zr + 1):
-
     for poor_coop in range(Zp + 1):
         i = [[rich_coop, Zr - rich_coop], [poor_coop, Zp - poor_coop]]
 
@@ -84,20 +88,30 @@ for rich_coop in range(Zr + 1):
                     if X == 0:
                         if rich_coop != 0:
                             T_k_XtoY = compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h)
-                            M[rich_coop * Zp + poor_coop][(rich_coop - 1) * Zp + poor_coop] = T_k_XtoY
-                    else:
+                            M[rich_coop * (Zp+1) + poor_coop][(rich_coop - 1) * (Zp+1) + poor_coop] = T_k_XtoY
+                    elif X == 1:
                         if rich_coop != Zr:
                             T_k_XtoY = compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h)
-                            M[rich_coop * Zp + poor_coop][(rich_coop + 1) * Zp + poor_coop] = T_k_XtoY
+                            M[rich_coop * (Zp+1) + poor_coop][(rich_coop + 1) * (Zp+1) + poor_coop] = T_k_XtoY
                 else:
                     if X == 0:
                         if poor_coop != 0:
                             T_k_XtoY = compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h)
-                            M[rich_coop * Zp + poor_coop][rich_coop * Zp + (poor_coop - 1)] = T_k_XtoY
-                    else:
+                            M[rich_coop * (Zp+1) + poor_coop][rich_coop * (Zp+1) + (poor_coop - 1)] = T_k_XtoY
+                    elif X == 1:
                         if poor_coop != Zp:
                             T_k_XtoY = compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h)
-                            M[rich_coop * Zp + poor_coop][rich_coop * Zp + (poor_coop + 1)] = T_k_XtoY
+                            M[rich_coop * (Zp+1) + poor_coop][rich_coop * (Zp+1) + (poor_coop + 1)] = T_k_XtoY
 
+w, v = np.linalg.eig(M.transpose())
 
-print(M[0])
+j_stationary = np.argmin(abs(w-1.0))
+
+p_stationary=abs(v[:, j_stationary].real)
+
+p_norm = p_stationary / p_stationary.sum()
+
+xs = [x for x in range(len(p_stationary))]
+
+plt.plot(xs, p_stationary)
+plt.show()
