464b4fdda9e5aea46e5e5ac5285d73a57274efc3
Sacha Duynslaeger <sacha.d@hotmail.com>
2024-01-06 11:28:02 +0100

    eta g done

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 from math import exp
-
+from scipy.stats import multivariate_hypergeom
 import numpy as np
 import sys
 
@@ -19,7 +19,7 @@ Z_tot = [Zr, Zp]
 N = 6
 mu = 1 / Z
 h = 1
-obstinate_r_c = 0.1
+obstinate_r_c = 0
 obstinate_p_c = 0
 obstinate_r_d = 0
 obstinate_p_d = 0
@@ -35,10 +35,10 @@ c_p = 0.1 * b_p
 b_bar = 1
 c_bar = 0.1
 
-M = 3
+M_thresh = 3
 
-Mcb_threshold = c_bar * M * b_bar
-r = 0.2
+Mcb_threshold = c_bar * M_thresh * b_bar
+r = 0.8
 
 T_coop_rich = 1
 T_defect_rich = 1
@@ -95,79 +95,89 @@ def compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h, obstinators):
                                 )
     return T_k_XtoY
 
+eta_g_over_risk = []
+x_axis = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+for ind_x in range(len(x_axis)):
+    r = x_axis[ind_x]
+    print("Percieved risk = ",r)
+    M = np.zeros([(Zp + 1) * (Zr + 1), (Zp + 1) * (Zr + 1)])
+    a_g = []
+    M_plot = np.zeros([Zp + 1, Zr + 1])
+    for rich_coop in tqdm(range(obstinate_coop[0], Zr + 1 - obstinate_defector[0])):
+        for poor_coop in range(obstinate_coop[1], Zp + 1 - obstinate_defector[1]):
+            i = [[rich_coop, Zr - rich_coop], [poor_coop, Zp - poor_coop]]
+            for k in range(nb_strat):
+                l = (k + 1) % nb_strat
+                for v in range(nb_strat):
+                    X = v
+                    Y = (v + 1) % nb_strat
+                    if k == 0:
+                        if X == 0:
+                            if rich_coop != 0:
+                                T_k_XtoY = compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h, obstinate_coop[0])
+                                M[rich_coop * (Zp + 1) + poor_coop][(rich_coop - 1) * (Zp + 1) + poor_coop] = T_k_XtoY
+                        elif X == 1:
+                            if rich_coop != Zr:
+                                T_k_XtoY = compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h, obstinate_defector[0])
+                                M[rich_coop * (Zp + 1) + poor_coop][(rich_coop + 1) * (Zp + 1) + poor_coop] = T_k_XtoY
+                    else:
+                        if X == 0:
+                            if poor_coop != 0:
+                                T_k_XtoY = compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h, obstinate_coop[1])
+                                M[rich_coop * (Zp + 1) + poor_coop][rich_coop * (Zp + 1) + (poor_coop - 1)] = T_k_XtoY
+                        elif X == 1:
+                            if poor_coop != Zp:
+                                T_k_XtoY = compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h, obstinate_defector[1])
+                                M[rich_coop * (Zp + 1) + poor_coop][rich_coop * (Zp + 1) + (poor_coop + 1)] = T_k_XtoY
 
-M = np.zeros([(Zp + 1) * (Zr + 1), (Zp + 1) * (Zr + 1)])
-a_g = []
-M_plot = np.zeros([Zp + 1, Zr + 1])
-for rich_coop in tqdm(range(obstinate_coop[0], Zr + 1 - obstinate_defector[0])):
-    for poor_coop in range(obstinate_coop[1], Zp + 1 - obstinate_defector[1]):
-        i = [[rich_coop, Zr - rich_coop], [poor_coop, Zp - poor_coop]]
-        for k in range(nb_strat):
-            l = (k + 1) % nb_strat
-            for v in range(nb_strat):
-                X = v
-                Y = (v + 1) % nb_strat
-                if k == 0:
-                    if X == 0:
-                        if rich_coop != 0:
-                            T_k_XtoY = compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h, obstinate_coop[0])
-                            M[rich_coop * (Zp + 1) + poor_coop][(rich_coop - 1) * (Zp + 1) + poor_coop] = T_k_XtoY
-                    elif X == 1:
-                        if rich_coop != Zr:
-                            T_k_XtoY = compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h, obstinate_defector[0])
-                            M[rich_coop * (Zp + 1) + poor_coop][(rich_coop + 1) * (Zp + 1) + poor_coop] = T_k_XtoY
-                else:
-                    if X == 0:
-                        if poor_coop != 0:
-                            T_k_XtoY = compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h, obstinate_coop[1])
-                            M[rich_coop * (Zp + 1) + poor_coop][rich_coop * (Zp + 1) + (poor_coop - 1)] = T_k_XtoY
-                    elif X == 1:
-                        if poor_coop != Zp:
-                            T_k_XtoY = compute_T(i, k, l, X, Y, Z, mu, beta, Z_tot, h, obstinate_defector[1])
-                            M[rich_coop * (Zp + 1) + poor_coop][rich_coop * (Zp + 1) + (poor_coop + 1)] = T_k_XtoY
+            M[rich_coop * (Zp + 1) + poor_coop][rich_coop * (Zp + 1) + poor_coop] = 1 - np.sum(
+                M[rich_coop * (Zp + 1) + poor_coop])
 
-        M[rich_coop * (Zp + 1) + poor_coop][rich_coop * (Zp + 1) + poor_coop] = 1 - np.sum(
-            M[rich_coop * (Zp + 1) + poor_coop])
+            rich_defectors = Zr - rich_coop
+            poor_defectors = Zp - poor_coop
+            multi_hypergeom_curr_composition = multivariate_hypergeom(m = [rich_coop, rich_defectors, poor_coop, poor_defectors], n = 6)
+            aG_i = 0.0
+            for rc in range(N): # rc represent the number of rich cooperator in the group
+                for pc in range(N) : # pc represent the number of poor cooperator in the group
+                    if(rc + pc) > M_thresh and(rc + pc) <= N: # if there are enough cooperators to reach the threshold
+                        for rd in range(N): # rd represent the number of rich defectors in the group
+                            if(rc + pc + rd <= N):
+                                for pd in range(N): # pd represent the number of poor defectors in the group
+                                    if(rc + pc + rd + pd == N):
+                                        pmf = multi_hypergeom_curr_composition.pmf(x=[rc, rd, pc, pd])
+                                        aG_i += pmf
+            a_g.append(aG_i)
 
-        # if poor_coop != 0 and rich_coop != 0 and rich_coop != Zr and poor_coop != Zp:
-        f_r_c = 0
-        f_r_d = 0
-        f_p_c = 0
-        f_p_d = 0
+    print("Compute eigen values")
+    w, v = np.linalg.eig(M.transpose())
+    print("Compute j_stationary")
+    j_stationary = np.argmin(abs(w - 1.0))
+    print("Compute p_stationary")
+    p_stationary = abs(v[:, j_stationary].real)
 
-        if rich_coop != 0 and rich_coop != Zr:
-            f_r_c = pgg_game.fraction_group("rich", "cooperator", i, Z, N, b_r, b_p, c_r, c_p, Mcb_threshold, r)
-            f_r_d = pgg_game.fraction_group("rich", "defector", i, Z, N, b_r, b_p, c_r, c_p, Mcb_threshold, r)
-        if poor_coop != 0 and poor_coop != Zp:
-            f_p_c = pgg_game.fraction_group("poor", "cooperator", i, Z, N, b_r, b_p, c_r, c_p, Mcb_threshold, r)
-            f_p_d = pgg_game.fraction_group("poor", "defector", i, Z, N, b_r, b_p, c_r, c_p, Mcb_threshold, r)
+    p_plot = 1 - (p_stationary / np.max(p_stationary))
+    p_norm = p_stationary / p_stationary.sum()
 
-        a_g.append(min(f_r_c, 1))
+    eta_g = 0
+    for i in range(len(a_g)):
+        eta_g += p_norm[i] * a_g[i]
 
-print("Compute eigen values")
-w, v = np.linalg.eig(M.transpose())
-print("Compute j_stationary")
-j_stationary = np.argmin(abs(w - 1.0))
-print("Compute p_stationary")
-p_stationary = abs(v[:, j_stationary].real)
+    print(eta_g)
+    eta_g_over_risk.append(eta_g)
 
-p_plot = 1 - (p_stationary / np.max(p_stationary))
 
-p_norm = p_stationary / p_stationary.sum()
+# xs = [x for x in range(len(p_stationary))]
+# plt.plot(xs, p_norm)
+# plt.show()
 
-for i in range(len(p_plot)):
-    M_plot[i % (Zp + 1)][i // (Zp + 1)] = p_plot[i]
+# plt.matshow(M_plot, cmap=plt.get_cmap('gray'), vmin=0.0, vmax=1.0)
+# plt.show()
+    
 
-xs = [x for x in range(len(p_stationary))]
-
-eta_g = 0
-for i in range(len(a_g)):
-    eta_g += p_norm[i] * a_g[i]
-
-print(eta_g)
-
-plt.plot(xs, p_norm)
+plt.plot(x_axis,eta_g_over_risk, color='r', label="h = 1")
+plt.ylim([0.0, 1.0])
+plt.xlabel("risk (r)")
+plt.ylabel("Group achievement ($\\eta_G$)")
+plt.legend()
 plt.show()
 
-plt.matshow(M_plot, cmap=plt.get_cmap('gray'), vmin=0.0, vmax=1.0)
-plt.show()
